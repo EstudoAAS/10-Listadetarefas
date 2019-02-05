f8970a58b81c9369c3420e9eb383906c7d4c699d
Aller Augusto de Souza <aller.souza@samsung.com>
2019-02-05 17:51:22 -0200

    Add menu item

--- app/src/main/java/com/hudeing/listadetarefas/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/com/hudeing/listadetarefas/activity/AdicionarTarefaActivity.java
@@ -2,6 +2,9 @@ package com.hudeing.listadetarefas.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.hudeing.listadetarefas.R;
 
@@ -11,5 +14,22 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemSalvar:
+                //Executa ação para o item salvar
+                Toast.makeText(AdicionarTarefaActivity.this, "Botão salvar pressionado", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
