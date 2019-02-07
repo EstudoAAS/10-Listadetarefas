f1d4e5ce32b9510868479c1824407a229dfa8100
Aller Augusto de Souza <aller.souza@samsung.com>
2019-02-07 16:05:46 -0200

    Add delete task

--- app/src/main/java/com/hudeing/listadetarefas/activity/MainActivity.java
+++ app/src/main/java/com/hudeing/listadetarefas/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package com.hudeing.listadetarefas.activity;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private TarefaAdapter tarefaAdapter;
     private List<Tarefa> listaTarefas = new ArrayList<>();
+    private Tarefa tarefaSelecionada;
 
 
     @Override
@@ -68,11 +71,40 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onLongItemClick(View view, int position) {
-                        Toast.makeText(
-                                getApplicationContext(),
-                                "onLongItemClick",
-                                Toast.LENGTH_SHORT
-                        ).show();
+
+                        //Recuperar tarefa para delecao
+                        tarefaSelecionada = listaTarefas.get(position);
+
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+
+                        //Configura título e mensagem
+                        dialog.setTitle("Confirmar exclusão");
+                        dialog.setMessage("Deseja excluir a tarefa: " + tarefaSelecionada.getNomeTarefa() + "?");
+
+                        dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                TarefaDAO tarefaDAO = new TarefaDAO(getApplicationContext());
+                                if ( tarefaDAO.deletar( tarefaSelecionada )) {
+                                    //recreate();
+                                    carregarListaTarefas();
+                                    Toast.makeText(getApplicationContext(),
+                                            "Sucesso ao excluir tarefa!",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Erro ao excluir tarefa!",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                        dialog.setNegativeButton("Não", null);
+
+                        //Exibir dialog
+                        dialog.create();
+                        dialog.show();
                     }
 
                     @Override
--- app/src/main/java/com/hudeing/listadetarefas/helper/TarefaDAO.java
+++ app/src/main/java/com/hudeing/listadetarefas/helper/TarefaDAO.java
@@ -56,7 +56,16 @@ public class TarefaDAO implements ITarefaDAO{
 
     @Override
     public boolean deletar(Tarefa tarefa) {
-        return false;
+        try {
+            String[] args = {tarefa.getId().toString()};
+            escreve.delete(DBHelper.TABELA_TAREFAS, "id=?", args);
+
+            Log.i("INFO", "Tarefa removida com sucesso!");
+        } catch (Exception e) {
+            Log.e("INFO", "Erro ao remover tarefa " + e.getMessage());
+            return false;
+        }
+        return true;
     }
 
     @Override
